e0679418a78ac3c3815409ec6da1cfc61847bbe3
JenniferJoseph98 <honeyjen1607@gmail.com>
2024-11-27 00:34:53 +0530

    Make image accessible through static

--- college/urls.py
+++ college/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('', views.index, name='index'),
     path('delete',views.DeleteAllStudentsAndSubjects.as_view(),name='delete'),
@@ -25,3 +26,6 @@ urlpatterns = [
     path('students/update-profile-pic/<str:studentId>', views.UpdateProfilePicView.as_view(), name='update-profile-pic'),
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
